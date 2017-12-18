34d77483e197104b43edff4ae78ea4a3b7c01598
Nattapat Niyomthai <34436384+NattapatN@users.noreply.github.com>
2017-12-18 14:43:26 +0700

    version 4.5

---
+++ Program/src/Test_Module/Test_File_Out.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test_Module;
+
+import java.io.*;
+/**
+ *
+ * @author NattapatN
+ */
+public class Test_File_Out {
+    
+    public static void main(String []args)throws IOException{
+        
+    }
+}
